3628f04a3758f0540066847d178a1bbe5c340750
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-05 20:43:21 +0600

    delete method

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/job/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
